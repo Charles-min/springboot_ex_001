545c4d185deadf5b7b7eee6841171ad5886f2539
Charles <59673870+Charles-min@users.noreply.github.com>
2021-10-13 20:39:48 +0900

    21.10.13 테스트 코드 연습

---
+++ src/test/java/com/jojoldu/book/springboot/ApplicationTest.java
@@ -0,0 +1,11 @@
+package com.jojoldu.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class ApplicationTest {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationTest.class, args);
+    }
+}
